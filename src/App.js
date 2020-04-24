e2f78e60bfcd70a77fca60b7f7b71e23ad1644c5
Drew Larson <me@drewlarson.ca>
2020-04-23 19:14:41 -0600

    Added a TypeInput component as well as 1. Added linting 2. Added Prettier 3. Removed unneeded dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TypeInput } from './TypeInput';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <main
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <TypeInput />
+        </main>
+    );
 }
 
 export default App;
---
+++ src/TypeInput.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const TypeInput = () => {
+    return (
+        <input
+            style={{ width: '80%', height: '40px', fontSize: '32px' }}
+            type="text"
+            placeholder='Get Ready!'
+        />
+    );
+};
